087362b4c99402b4e877a60123c346cb394ce038
Michelle Holland <michellebholland@gmail.com>
2020-09-08 18:52:27 -0700

    feat: install express and add error handlers

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
-import { typeDefs, resolvers } from './index';
+import { typeDefs, resolvers } from './schema/index';
 
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
 const { ApolloServer } = require('apollo-server-express');
 
 const PORT = 4000;
@@ -16,6 +14,21 @@ const server = new ApolloServer({
 });
 // Apply express as middleware
 server.applyMiddleware({ app, path: '/graphql' });
+
+// Error handler - 404 catch-all
+server.use('*', (req, res) => res.sendStatus(404));
+
+// Error handler – global 
+server.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = { ...defaultErr, ...err };
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 server.listen({ port: PORT }, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}`);
