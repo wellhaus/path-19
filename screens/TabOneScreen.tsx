9649121a67f7886e4a112aa97c6944d5c7041a91
haejinjo <h3jnjo@gmail.com>
2020-09-10 13:56:17 -0700

    refactor: begin linking to backend data types and prep for luis pull

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -69,22 +69,93 @@ export default function TabOneScreen() {
         setErrorMsg('Permission to access location was denied');
       }
       setLocation(await Location.getCurrentPositionAsync({}));
-    })();
 
-    // Populate locations array with all data form server
-    // TODO: lazy load locations
-    // Create local cache
-    // Create key using parameters of each new fetch for locations
-    // Store in state along with queried region
-    // Check if last call was done with the same key and with a region wrapping current one
-    // If so, we alreday have all the points to display so ignore request
-    setAllLocations([{ name: 'Dallas', lat: 32.7767, long: -96.7970, timestamp: 1599670918, proximity: 0 },
-    { name: 'Salt & Straw Venice', lat: 33.9908, long: -118.4660, timestamp: 1599670918, proximity: 0 },
-    { name: 'Miami', lat: 25.7617, long: -80.1918, timestamp: 1599670918, proximity: 0 },
-    { name: 'San Francisco', lat: 37.7749, long: -122.4194, timestamp: 1599670918, proximity: 0 }
-    ]);
+      // Populate locations array with all data form server
+      // TODO: lazy load locations
+      // Create local cache
+      // Create key using parameters of each new fetch for locations
+      // Store in state along with queried region
+      // Check if last call was done with the same key and with a region wrapping current one
+      // If so, we alreday have all the points to display so ignore request
+      // setAllLocations([{ name: 'Dallas', lat: 32.7767, long: -96.7970, timestamp: 1599670918, proximity: 0 },
+      // { name: 'Salt & Straw Venice', lat: 33.9908, long: -118.4660, timestamp: 1599670918, proximity: 0 },
+      // { name: 'Miami', lat: 25.7617, long: -80.1918, timestamp: 1599670918, proximity: 0 },
+      // { name: 'San Francisco', lat: 37.7749, long: -122.4194, timestamp: 1599670918, proximity: 0 }
+      // ]);
+      setAllLocations([
+        {
+          _id: 1,
+          name: "Starbucks",
+          longitude: 37.4224764,
+          latitude: -122.0842499,
+          onset: "March 23, 2020",
+          date_visited: "March 20, 2020",
+          proximity: 0,
+        },
+        {
+          _id: 2,
+          name: "Alfred Coffee",
+          longitude: 39.4224764,
+          latitude: -192.0842499,
+          onset: "April 23, 2020",
+          date_visited: "April 13, 2020",
+          proximity: 0,
+        },
+        {
+          _id: 3,
+          name: "Rubios",
+          longitude: 17.4224764,
+          latitude: -102.0842499,
+          onset: "July 23, 2020",
+          date_visited: "July 8, 2020",
+          proximity: 0,
+        },
+        {
+          _id: 4,
+          name: "Sidecar Doughnuts & Coffee",
+          longitude: 87.4224764,
+          latitude: -162.0842499,
+          onset: "August 23, 2020",
+          date_visited: "August 13, 2020",
+          proximity: 0,
+        },
+        {
+          _id: 5,
+          name: "Salt & Straw",
+          longitude: 31.4224764,
+          latitude: -145.0842499,
+          onset: "March 23, 2020",
+          date_visited: "March 20, 2020",
+          proximity: 0,
+        },
+        {
+          _id: 6,
+          name: "Blue Bottle Coffee",
+          longitude: 30.4224764,
+          latitude: -180.0842499,
+          onset: "April 23, 2020",
+          date_visited: "April 13, 2020",
+          proximity: 0,
+        }]);
+    })();
   }, []);
 
+
+
+
+  //  query GetLocations {
+  //      locations {
+  //    			_id
+  //    			name
+  //    			longitude
+  //    			latitude
+  //    			onset
+  //    			dateVisited
+  //      }
+  //    }
+
+  console.log("LOCATION: ", location);
+  console.log("LOCATINS ALL: ", allLocations);
   return !location ?
     <LoadingView /> :
     errorMsg ?
@@ -100,10 +171,10 @@ export default function TabOneScreen() {
               longitudeDelta: 0.05
             }}
           >
-            {allLocations.map(({ name, lat, long }, index) => (
+            {allLocations.map(({ _id, name, latitude, longitude }) => (
               <Marker
-                coordinate={{ latitude: lat, longitude: long }}
-                key={"markerKey" + index}
+                coordinate={{ latitude: longitude, longitude: latitude }}
+                key={"markerKey" + _id}
                 image={require('../assets/images/redAuraMarker.png')}
               >
                 <Callout tooltip>
