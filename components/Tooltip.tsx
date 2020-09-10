cd2381c1311b6111fd129814843be410d43e6098
haejinjo <h3jnjo@gmail.com>
2020-09-09 22:51:02 -0700

    feat: component to contain framed view in callout above pin

---
+++ components/Tooltip.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Text, View } from './Themed';
+import * as Location from 'expo-location';
+
+export default function Tooltip({ timestamp }: Location.LocationData) {
+  return (
+    <View>
+      <View style={styles.calloutText}>
+        <Text>{"Confirmed Patient"}</Text>
+        <Text>{"Last visited: " + new Date(timestamp)}</Text>
+        <Text>{"TODO: Add more specific building name"}</Text>
+      </View>
+      <View style={styles.arrow} />
+      <View style={styles.arrowBorder} />
+    </View >
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  calloutText: {
+    flexDirection: 'column',
+    borderRadius: 6,
+  },
+  // Arrow below the bubble
+  arrow: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#fff',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -32,
+  },
+  arrowBorder: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#007a87',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -0.5,
+    // marginBottom: -15
+  },
+});
