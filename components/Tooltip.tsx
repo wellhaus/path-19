80aeca3d039a52e93a4eff81f05cd58c004a4498
haejinjo <h3jnjo@gmail.com>
2020-09-10 02:09:56 -0700

    feat: enable error checking and conditional rendering for Tooltip

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, View } from './Themed';
 import * as Location from 'expo-location';
 
 export interface TooltipProps {
   location: Location.LocationData,
+  placeName: string,
 };
 
-export default function Tooltip({ location }: TooltipProps) {
-  const { timestamp } = location;
+export default function Tooltip({ location, placeName }: TooltipProps) {
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [address, setAddress] = useState<Location.Address | null>(null);
 
-  return (
-    <View>
-      <View style={styles.calloutText}>
-        <Text>{"Confirmed Patient"}</Text>
-        <Text>{"Last visited: " + new Date(timestamp)}</Text>
-        <Text>{"TODO: Add more specific building name"}</Text>
+  const getReadableLocation = async ({ coords }: Location.LocationData): Location.Address => {
+    const { latitude, longitude } = coords;
+    try {
+      const readableLocation = await Location.reverseGeocodeAsync({ latitude, longitude });
+      return readableLocation[0];
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      // Get readable postal address from current geocoded location
+      try {
+        setAddress(await getReadableLocation(location));
+        // console.log(`${name} ${city} ${region} ${postalCode} ${country}`);
+      } catch (err) {
+        setErrorMsg(err);
+      }
+    })();
+  }, []);
+
+  return !address ?
+    (
+      <View style={styles.bubble}>
+        <Text>{"..."}</Text>
       </View>
-      <View style={styles.arrow} />
-      <View style={styles.arrowBorder} />
-    </View >
-  );
+    ) :
+    errorMsg ?
+      (
+        <View>
+          <View style={styles.bubble}>
+            <Text>{errorMsg}</Text>
+          </View>
+        </View>
+      ) :
+      (
+        <View >
+          <View style={styles.bubble}>
+            <Text>{"Confirmed Patient"}</Text>
+            <Text>{"Last visited: " + new Date(location.timestamp)}</Text>
+            <Text>{placeName}</Text>
+          </View>
+          <View style={styles.arrowBorder} />
+          <View style={styles.arrow} />
+        </View >
+      );
+
 }
 
 
@@ -30,16 +69,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  calloutText: {
+  // Callout bubble
+  bubble: {
     flexDirection: 'column',
+    alignSelf: 'flex-start',
+    backgroundColor: '#fff',
     borderRadius: 6,
+    borderColor: '#ccc',
+    borderWidth: 0.5,
+    padding: 15,
+    width: 200,
   },
   // Arrow below the bubble
   arrow: {
     backgroundColor: 'transparent',
     borderColor: 'transparent',
     borderTopColor: '#fff',
-    borderWidth: 16,
+    borderWidth: 5,
     alignSelf: 'center',
     marginTop: -32,
   },
